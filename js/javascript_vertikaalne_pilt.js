49101075bd2acd08450c33e8e5ddf9e5f9530833
ArturKreegipuu <artur@kreegipuu.net>
2021-11-29 21:57:35 +0200

    viide

--- js/javascript_vertikaalne_pilt.js
+++ js/javascript_vertikaalne_pilt.js
@@ -5,3 +5,4 @@ function myFunction(imgs) {
     imgText.innerHTML = imgs.alt; //suurendatud pildi juures asuv tekst tuleb pildi "alt" atribuudilt
     expandImg.parentElement.style.display = "block"; //näitab suurendatud pilti plokk-stiilis
   }
+// Piltide galerii JavaScript pärineb: https://www.w3schools.com/howto/howto_js_tab_img_gallery.asp
